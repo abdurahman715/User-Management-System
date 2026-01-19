1fdedbf732fb90fec05b8f914e73046b6c997c58
Muhammed Abdurahman C K <abdurahmanck715@gmail.com>
2026-01-19 10:15:12 +0530

    ..

--- config/config.js
+++
@@ -1,4 +0,0 @@
-const sessionSecret = "mysessionsecret";
-const emailUser = "merntestingpurpose@gmail.com";
-const emailPassword = "frny dbvd mwsp vqbz";
-module.exports = { sessionSecret, emailUser, emailPassword };
